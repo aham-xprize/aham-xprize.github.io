474be002e260dd0288b7a1bd0478050c2b94867d
Rushali Mohbe <rushali17mohbe@gmail.com>
2021-09-16 22:07:47 +0530

    Update operator.js

--- operator.js
+++ operator.js
@@ -56,7 +56,7 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('player2', {
         height: '390',
         width: '640',
-        videoId: 'v6zeJjSWimY',
+        videoId: 'PjSu8-8sjQc',
         playerVars: {
             'playsinline': 1
         },
